35184729e37c421e3ca42b283e4975d4982a7f8d
dythirk <dythirk@github.com>
2025-12-31 23:20:19 +0000

    Test run: 1/3 passed, 2 failed, 0 skipped - 2025-12-31T23:20:19.777Z

--- src/main/java/Fibonacci.java
+++ src/main/java/Fibonacci.java
@@ -15,6 +15,17 @@ public class Fibonacci {
      * @return the nth number of fibonacci sequence.
      */
     public int fib(int n){
-        return 0;
+        int fibo = 0;
+        int a = 1;
+        if (n == 1) {
+            return fibo;
+        }
+        else
+            for (int i = 2; i <= n; i++) {
+                fibo += a;
+                a = fibo;
+
+            }
+        return fibo;
     }
 }
